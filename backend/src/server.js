c3858d7f3a71d9d8ecf79b31ee422337cfacf6d0
burakorkmez <burakorkmezz@gmail.com>
2025-06-26 07:49:01 +0300

    comment routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -19,6 +20,7 @@ app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
